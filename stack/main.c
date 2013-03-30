18a35bd578818f7d833bc0e96e2ba568fa430dfe
gavinlin <261878441@qq.com>
2013-03-30 16:51:44 +0800

    use stack matching

--- stack/main.c
+++ stack/main.c
@@ -3,6 +3,103 @@
 #include "linkstack.h"
 #include <math.h>
 
+int isLeft(char c){
+	int ret = FALSE;
+	switch(c){
+	case '<':
+	case '{':
+	case '[':
+	case '(':
+		ret = TRUE;
+		break;
+	default:
+		ret = FALSE;
+		break;
+	}
+	return ret;
+}
+
+int isRight(char c){
+	int ret = FALSE;
+	switch(c){
+	case '>':
+	case '}':
+	case ']':
+	case ')':
+		ret = TRUE;
+		break;
+	default:
+		ret = FALSE;
+		break;
+	}
+	return ret;
+}
+
+Status isMatch(char left, char right){
+	int ret = FALSE;
+	switch(left){
+	case '<':
+		if( right == '>'){
+			ret = TRUE;
+		}
+		break;
+	case '{':
+		if( right == '}'){
+			ret = TRUE;
+		}
+		break;
+	case '[':
+		if( right == ']'){
+			ret = TRUE;
+		}
+		break;
+	case '(':
+		if( right == ')'){
+			ret = TRUE;
+		}
+		break;
+	default:
+		ret = FALSE;
+		break;
+	}
+	return ret;
+
+}
+
+Status scanner(char* c){
+	LinkStack* stack = NULL;
+	InitStack(&stack);
+	Status ret = FALSE;
+	int i = 0;
+	while(c[i] != '\0'){
+		elemType e;
+		if( isLeft(c[i]) ){
+			Push(stack,(elemType)c[i]);
+		}
+		if(isRight(c[i])){
+			char codeOut;
+			Pop(stack,&e);
+			codeOut = (char)e;
+			if(!isMatch(codeOut,c[i])){
+				printf("not matching %c %c\n",codeOut,c[i]);
+				ret = FALSE;
+				goto EXIT;
+			}
+		}
+		i++;
+	}
+
+	if(StackEmpty(stack)){
+		ret = TRUE;
+	}else{
+		ret = FALSE;
+	}
+
+EXIT:
+	ClearStack(&stack);
+	return ret;
+}
+
 int main(int argc, const char *argv[])
 {
 	LinkStack *testList = NULL;
@@ -22,5 +119,8 @@ int main(int argc, const char *argv[])
 	printf("pop e is %d ,length is %d \n",e,StackLength(testList));
 	ClearStack(&testList);
 	printf("list length is %d\n",StackLength(testList));
+
+	printf("scanner: is it matching? ");
+	scanner("hello(){}") ? printf("true \n"):printf("false \n");
 	return 0;
 }
